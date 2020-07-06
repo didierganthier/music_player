814a87f024e2742680efaef6ea029d2f26497bce
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-07-06 15:08:37 -0400

    Navigation setup with bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:musicplayer/screens/home.dart';
+import 'package:musicplayer/screens/upload.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentIndex = 0;
+
+  List tabs = [
+    Home(),
+    Upload()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,8 +31,24 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Music Player App'),
         ),
-        body: Center(
-          child: Text('This is Home Page'),
+        body: tabs[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home')
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.cloud_upload),
+                title: Text('Upload')
+            ),
+          ],
+          onTap: (index){
+            setState(() {
+              currentIndex = index;
+            });
+          },
         ),
       ),
     );
