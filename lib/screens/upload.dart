2562715094995524a1f1d214be662ab6ab205718
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-07-07 01:13:22 -0400

    upload.dart page basic UI completed

--- lib/screens/upload.dart
+++ lib/screens/upload.dart
@@ -6,10 +6,46 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  TextEditingController songName = TextEditingController();
+  TextEditingController artistName = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('This is Upload Page'),
+      child: Column(
+        children: <Widget>[
+          RaisedButton(
+            onPressed: (){},
+            child: Text('Select Image'),
+          ),
+          RaisedButton(
+            onPressed: (){},
+            child: Text('Select Song'),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            child: TextField(
+              controller: songName,
+              decoration: InputDecoration(
+                hintText: 'Enter song name'
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            child: TextField(
+              controller: artistName,
+              decoration: InputDecoration(
+                  hintText: 'Enter artist name'
+              ),
+            ),
+          ),
+          RaisedButton(
+            onPressed: (){},
+            child: Text('Upload'),
+          )
+        ],
+      ),
     );
   }
 }
