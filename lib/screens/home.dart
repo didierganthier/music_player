0584f6aa2007bdbe064e42d52c4c7866592884fb
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-07-08 08:26:04 -0400

    songs titles displayed in home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,10 +7,39 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Future getData() async {
+    QuerySnapshot qn = await Firestore.instance.collection('songs').getDocuments();
+    return qn.documents;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('This is Home Page'),
+    return FutureBuilder(
+      future: getData(),
+      builder: (context, snapshot){
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index){
+              return InkWell(
+                onTap: ()=> {},
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(snapshot.data[index].data['song_name'], style: TextStyle(fontSize: 20.0)),
+                  ),
+                  elevation: 1,
+                ),
+              );
+            },
+          );
+        }
+      }
     );
   }
 }
